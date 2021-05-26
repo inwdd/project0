c2cf46377259df07af3bb19ba91ca167594db977
inwdd <84837293+inwdd@users.noreply.github.com>
2021-05-26 04:01:17 -0400

    Add files via upload

---
+++ maps-project/maindata.js
@@ -0,0 +1,511 @@
+var projectSites = [
+    {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [35.388162, 14.044225]
+        },
+        "properties": {
+          "name": "Gadarief, SU",
+          "info": "Gadarief Text",
+          "boxcontent": "lib/zgadarief.png",
+          "countryflag": "<img height=12px src=lib/cSUbig.png>  ",
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [36.149322, 12.960314]
+        },
+        "properties": {
+          "name": "Galabat, SU",
+          "info": "Info on Galabat. Shop No 7, Ground Floor, Chemox House, Hospital Lane, Barrack Rd, New Marine Lines, Mumbai, Maharashtra 400020, India",
+          "boxcontent": "lib/zgalabat.png",
+          "countryflag": "<img height =12px src=lib/cSUbig.png> ",
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [36.413976, 15.455983]
+        },
+        "properties": {
+          "name": "Hamdayet, SU",
+          "info": "Hamdayet Info goes here!",
+          "boxcontent": "lib/zhamdayet.png",
+          "countryflag": "<img height =12px src=lib/cSUbig.png> ",
+          
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [33.020113, 12.526098]
+        },
+        "properties": {
+          "name": "Jodah, SU",
+          "info": "Jodah Info. Lower Parel, Mumbai, Maharashtra 400013, India",
+          "boxcontent": "lib/zjodah.png",
+          "countryflag": "<img height =12px src=lib/cSUbig.png> ",
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [32.244053, 14.165762]
+        },
+        "properties": {
+          "name": "White Nile, SU",
+          "info": "White Nile Info. Shop No 2, Ground floor Kanakia Zillion, LBS Marg, Kurla West, Mumbai, Maharashtra 400070, India",
+          "boxcontent": "lib/zwhitenile.png",
+          "countryflag": "<img height =12px src=lib/cSUbig.png> ",
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [31.598537, 4.851911]
+        },
+        "properties": {
+          "name": "Juba, SSU",
+          "info": "Juba Text goes here. No. 1-5, Building, Shop, No. 35, Evershine Millennium Paradise, Thakur Village, Kandivali East, Mumbai, Maharashtra 400101, India",
+          "boxcontent": "lib/zjuba.png",
+          "countryflag": "<img height =12px src=lib/cSSUbig.png> ",
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [33.040016, 10.053668]
+        },
+        "properties": {
+          "name": "Maban, SSU",
+          "info": "Maban Text: Floor,Shop No. A,Municipal No. 26A & 27A-UA Jawahar Nagar,Kamla Nagar, near Malka Ganj, Chowk, Delhi 110007, India",
+          "boxcontent": "lib/zmaban.png",
+          "countryflag": "<img height =12px src=lib/cSSUbig.png> ",
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [34.283834, 4.405186]
+        },
+        "properties": {
+          "name": "Nadapal, SSU",
+          "info": "SHOP NO. 10-11, COMMUNITY CENTRE, Garg Mall, Pocket 4, Sector 11, Rohini, New Delhi, Delhi 110020, India",
+          "boxcontent": "lib/znadapal.png",
+          "countryflag": "<img height =12px src=lib/cSSUbig.png> ",
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [32.068238, 3.586098]
+        },
+        "properties": {
+          "name": "Nimule, SSU",
+          "info": "Nimule Text. Plot No. 7,LSC, Pankaj Plaza, near Prince Apartments, I.P.Extension, Patparganj, New Delhi, Delhi 110092, India",
+          "boxcontent": "lib/znimule.png",
+          "countryflag": "<img height =12px src=lib/cSSUbig.png> ",
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [32.797374, 11.835407]
+        },
+        "properties": {
+          "name": "Renk, SSU",
+          "info": "Renk - No.16, Ground Floor, Community Center Rd, Pocket A, Okhla Phase I, Okhla Industrial Area, New Delhi, Delhi 110020, India",
+          "boxcontent": "lib/zrenk.png",
+          "countryflag": "<img height =12px src=lib/cSSUbig.png> ",
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [31.790426, 3.375915]
+        },
+        "properties": {
+          "name": "Adjumani, UG",
+          "info": "Adjumani Text Ground Floor, Shop no. 3 & 4, Dist, Chinar Park, Noapara, Sukanta Pally, Newtown, Kolkata, West Bengal 700157, India",
+          "boxcontent": "lib/zadjumani.png",
+          "countryflag": "<img height =12px src=lib/cUGbig.png> ",
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [34.090577, 0.468229]
+        },
+        "properties": {
+          "name": "Busia, UG",
+          "info": "Busia 3A, Humayun Place Near New Empire Cinema, Esplanade, New Market Area, Dharmatala, Kolkata, West Bengal 700087, India",
+          "boxcontent": "lib/zbusia.png",
+          "countryflag": "<img height =12px src=lib/cUGbig.png> ",
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [32.071642, 3.563650]
+        },
+        "properties": {
+          "name": "Elegu, UG",
+          "info": "Elegu : Municipality, 352/1889, Dakshminayan Barasat Road Under Panhihati, Sodepur, Kolkata, West Bengal 700110, India",
+          "boxcontent": "lib/zelegu.png",
+          "countryflag": "<img height =12px src=lib/cUGbig.png> ",
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [34.263055, 0.641313]
+        },
+        "properties": {
+          "name": "Malaba, UG",
+          "info": "Malaba Plot No. 99A, Block F, Nalini Ranjan Ave, New Alipore, Kolkata, West Bengal 700053, India",
+          "boxcontent": "lib/zmalaba.png",
+          "countryflag": "<img height =12px src=lib/cUGbig.png> ",
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [40.305615, 0.057190]
+        },
+        "properties": {
+          "name": "Daadab, KE",
+          "info": "Daadab Plot No. 50 & 51, Survey No. 301/2, 313/1, 304/2, Shop No. G - 7 & 8 Ground Floor, Opp. Gujarat High Court, S. G. Road Town Planning Scheme No. 28, Ahmedabad, Gujarat 380061, India",
+          "boxcontent": "lib/zdaadab.png",
+          "countryflag": "<img height =12px src=lib/cKEbig.png> ",
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [40.959344, 0.994737]
+        },
+        "properties": {
+          "name": "Diif, KE",
+          "info": "Diif (Kenya) Neelgagan Plaza, 1st And 2nd Floor Survey Number 2259 Commissioner Office, Dr Ambedkar Rd, Opposite Police, Jain Colony, Shahibag, Ahmedabad, Gujarat 380004, India",
+          "boxcontent": "lib/zdiif.png",
+          "countryflag": "<img height =12px src=lib/cKEbig.png> ",
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [39.641166, -0.455128]
+        },
+        "properties": {
+          "name": "Garissa, KE",
+          "info": "Garissa A1 Amrapali Axiom, Sardar Patel Ring Rd, Ambli, Ahmedabad, Gujarat 380058, India",
+          "boxcontent": "lib/zgarissa.png",
+          "countryflag": "<img height =12px src=lib/cKEbig.png> ",
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [41.857806, 3.939194]
+        },
+        "properties": {
+          "name": "Mandera, KE",
+          "info": "Mandera Shop Number A/03 & A/04, Ground Floor, Gujarat 382350, India",
+          "boxcontent": "lib/zmandera.png",
+          "countryflag": "<img height =12px src=lib/cKEbig.png> ",
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [37.991614, 2.339366]
+        },
+        "properties": {
+          "name": "Marsabit, KE",
+          "info": "Marsabit Shop no 31/A, Ground floor, Shoppers Orbit, Next Mahalaxsmi Complex Near Visharantwadi Police Chowki, Dhanori Rd, Pune, Maharashtra 411015, India",
+          "boxcontent": "lib/zmarsabit.png",
+          "countryflag": "<img height =12px src=lib/cKEbig.png> ",
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [35.639530, 3.446768]
+        },
+        "properties": {
+          "name": "Turkana, KE",
+          "info": "Turkana Shop No:4,5,14,15,16 & 17 Fortune Plaza, Hadapsar, Pune, Maharashtra 411028, India",
+          "boxcontent": "lib/zturkana.png",
+          "countryflag": "<img height =12px src=lib/cKEbig.png> ",
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [43.644664, 3.121935]
+        },
+        "properties": {
+          "name": "Baidoa, SO",
+          "info": "Baidoa Plot No. 7, Dhanlaxmi Society, Opp. Jagtap Hospital, Pune, Maharashtra 411051, India",
+          "boxcontent": "lib/zbaidoa.png",
+          "countryflag": "<img height =12px src=lib/cSObig.png> ",
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [41.874911, 3.924454]
+        },
+        "properties": {
+          "name": "Belet Hawa, SO",
+          "info": "Belet Hawa Bus Stop, Shop No. 6 & 7, opposite Deccan Gymkhana, Pune, Maharashtra 411004, India",
+          "boxcontent": "lib/zbelethawa.png",
+          "countryflag": "<img height =12px src=lib/cSObig.png> ",
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [49.180776, 11.282172]
+        },
+        "properties": {
+          "name": "Bosasso, SO",
+          "info": "Bosasso Cipla Foundation, Survey No. 116/1, Shop No. 2, Opposite, Warje, Pune, Maharashtra 411058, India",
+          "boxcontent": "lib/zbosasso.png",
+          "countryflag": "<img height =12px src=lib/cSObig.png> ",
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [41.010596, 0.413374]
+        },
+        "properties": {
+          "name": "Dhoblei, SO",
+          "info": "Dhoblei Dukes Complex, 120, Bharathiar Salai, Melapudur, Cantonment, Tiruchirappalli, Tamil Nadu 620001, India",
+          "boxcontent": "lib/zdhoblei.png",
+          "countryflag": "<img height =12px src=lib/cSObig.png> ",
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [40.964303, 0.994551]
+        },
+        "properties": {
+          "name": "Diif, SO",
+          "info": "Diif (Somalia) Vignesh Plaza, A-6, Ground Floor, Thillai Nagar Main Rd, Tiruchirappalli, Tamil Nadu 620018, India",
+          "boxcontent": "lib/zdiif2.png",
+          "countryflag": "<img height =12px src=lib/cSObig.png> ",
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [45.337671, 2.037124]
+        },
+        "properties": {
+          "name": "Mogadishu, SO",
+          "info": "Mogadishu Shop No. G5, G6 and G7, Ground Floor, Jaipur Rd, Hathi Bhata, Ajmer, Rajasthan 305001, India",
+          "boxcontent": "lib/zmogadishu.png",
+          "countryflag": "<img height =12px src=lib/cSObig.png> ",
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [43.335718, 9.602048]
+        },
+        "properties": {
+          "name": "Wajale, SO",
+          "info": "Wajale Domino'S Pizza Ground Floor Shop No 2 Shiv Mandir cinema, Fatehpur Rd, Sikar, Rajasthan 332001, India",
+          "boxcontent": "lib/zwajale.png",
+          "countryflag": "<img height =12px src=lib/cSObig.png> ",
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [42.704840, 11.154281]
+        },
+        "properties": {
+          "name": "Ali Sabieh, DJ",
+          "info": "Ali Sabieh Khasra No. 489 & 490, V Square Mall, Alwar Bypass Road, Sector-15, Bhiwadi, Rajasthan 301019, India",
+          "boxcontent": "lib/zalisabieh.png",
+          "countryflag": "<img height =12px src=lib/cDJbig.png> ",
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [43.142559, 11.608643]
+        },
+        "properties": {
+          "name": "Djibouti Port, DJ",
+          "info": "Djibouti Port A-47, Jhalawar Road, Indraprastha Industrial Area, Kota, Rajasthan 324005, India",
+          "boxcontent": "lib/zdjiboutiport.png",
+          "countryflag": "<img height =12px src=lib/cDJbig.png> ",
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [42.476356, 12.150212]
+        },
+        "properties": {
+          "name": "Dora, DJ",
+          "info": "Dora A-47, Jhalawar Road, Indraprastha Industrial Area, Kota, Rajasthan 324005, India",
+          "boxcontent": "lib/zdora.png",
+          "countryflag": "<img height =12px src=lib/cDJbig.png> ",
+         }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [41.837472, 11.717967]
+        },
+        "properties": {
+          "name": "Galafi, DJ",
+          "info": "Galafi A-47, Jhalawar Road, Indraprastha Industrial Area, Kota, Rajasthan 324005, India",
+          "boxcontent": "lib/zgalafi.png",
+          "countryflag": "<img height =12px src=lib/cDJbig.png> ",
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [43.293760, 11.964294]
+        },
+        "properties": {
+          "name": "Obock, DJ",
+          "info": "Obock A-47, Jhalawar Road, Indraprastha Industrial Area, Kota, Rajasthan 324005, India",
+          "boxcontent": "lib/zobock.png",
+          "countryflag": "<img height =12px src=lib/cDJbig.png> ",
+        }
+    },
+    {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [43.287527, 11.964997]
+        },
+        "properties": {
+          "name": "Obock (Merkazi), DJ",
+          "info": "Obock (Merkazi) A-47, Jhalawar Road, Indraprastha Industrial Area, Kota, Rajasthan 324005, India",
+          "boxcontent": "lib/zobock2.png",
+          "countryflag": "<img height =12px src=lib/cDJbig.png> ",
+        }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [34.591613, 8.248250]
+      },
+      "properties": {
+        "name": "Gambela, ET",
+        "info": "Gambela Text A-47, Jhalawar Road, Indraprastha Industrial Area, Kota, Rajasthan 324005, India",
+        "boxcontent": "lib/zgambela.png",
+        "countryflag": "<img height =12px src=lib/cETbig.png> ",
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [41.736924, 11.876704]
+      },
+      "properties": {
+        "name": "Hawli, ET",
+        "info": "Hawli Text A-47, Jhalawar Road, Indraprastha Industrial Area, Kota, Rajasthan 324005, India",
+        "boxcontent": "lib/zhawli.png",
+        "countryflag": "<img height =12px src=lib/cETbig.png> ",
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [36.152759, 12.958203]
+      },
+      "properties": {
+        "name": "Metema, ET",
+        "info": "Metema A-47, Jhalawar Road, Indraprastha Industrial Area, Kota, Rajasthan 324005, India",
+        "boxcontent": "lib/zmetema.png",
+        "countryflag": "<img height =12px src=lib/cETbig.png> ",
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [39.052264, 3.530475]
+      },
+      "properties": {
+        "name": "Moyale, ET",
+        "info": "Moyale A-47, Jhalawar Road, Indraprastha Industrial Area, Kota, Rajasthan 324005, India",
+        "boxcontent": "lib/zmoyale.png",
+        "countryflag": "<img height =12px src=lib/cETbig.png> ",
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [41.006512, 11.794918]
+      },
+      "properties": {
+        "name": "Semera, ET",
+        "info": "Semera A-47, Jhalawar Road, Indraprastha Industrial Area, Kota, Rajasthan 324005, India",
+        "boxcontent": "lib/zsemera.png",
+        "countryflag": "<img height =12px src=lib/cETbig.png> ",
+      }
+    },
+    {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [43.182245, 9.500747]
+      },
+      "properties": {
+        "name": "Tog Wajale, ET",
+        "info": "Tog Wajale A-47, Jhalawar Road, Indraprastha Industrial Area, Kota, Rajasthan 324005, India",
+        "boxcontent": "lib/ztogwajale.png",
+        "countryflag": "<img height =12px src=lib/cETbig.png> ",
+      }
+    },
+
+]
